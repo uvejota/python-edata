45b0c6445644fdee2afc833c77bb2847d1ab254b
Vicente Mayor <vmayor93@gmail.com>
2021-08-11 11:51:09 +0200

    args expansion to async_update

--- edata/helpers.py
+++ edata/helpers.py
@@ -67,7 +67,7 @@ class ReportHelper ():
         _LOGGER.debug ('ReportHelper: requesting an async update')
         if self.__loop is None:
             self.__loop = asyncio.get_event_loop()
-        self.__loop.run_in_executor(None, self.update, date_from, date_to)
+        self.__loop.run_in_executor(None, self.update, *[date_from, date_to])
 
     def update (self, date_from=datetime(1970, 1, 1), date_to=datetime.today()):
         _LOGGER.debug ('ReportHelper: requesting an update')
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/uvejota/python-edata'
 EMAIL = 'vmayorg@outlook.es'
 AUTHOR = 'VMG'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.5'
+VERSION = '0.1.6'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
