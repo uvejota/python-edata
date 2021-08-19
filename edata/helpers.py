e817b7fa785fdfe3ab517a27e3f7b797f118c0f7
Vicente Mayor <vmayor93@gmail.com>
2021-08-19 16:23:33 +0200

    new experimental features: BillingProcessor, idle load estimation; general enhancements; adding some group_by data to ReportHelper.data as 'consumptions_daily_sum' and 'consumptions_monthly_sum'

--- edata/helpers.py
+++ edata/helpers.py
@@ -4,9 +4,11 @@ from dateutil.relativedelta import relativedelta
 from edata.connectors import *
 from edata.processors import *
 import asyncio
+from aiopvpc import PVPCData, TARIFFS
+import tzlocal
+import pytz as tz
 
 _LOGGER = logging.getLogger(__name__)
-logging.basicConfig(level=logging.WARNING)
 
 PLATFORMS = ['datadis']
 ATTRIBUTES = {
@@ -23,12 +25,15 @@ ATTRIBUTES = {
     "month_p1_kWh": 'kWh',
     "month_p2_kWh": 'kWh',
     "month_p3_kWh": 'kWh',
+    "*month_pvpc_€": '€',
     "last_month_kWh": 'kWh',
     "last_month_daily_kWh": 'kWh',
     "last_month_days_kWh": 'd',
     "last_month_p1_kWh": 'kWh',
     "last_month_p2_kWh": 'kWh',
     "last_month_p3_kWh": 'kWh',
+    "*last_month_pvpc_€": '€',
+    "*last_month_idle_W": 'W',
     "max_power_kW": 'kW',
     "max_power_date": None,
     "max_power_mean_kW": 'kW',
@@ -47,24 +52,39 @@ class ReportHelper ():
 
     data = {}
     attributes = {}
+    status = {}
 
-    def __init__(self, platform, username, password, cups, log_level=logging.WARNING) -> None:
+    def __init__(self, platform, username, password, cups, experimental=False, log_level=logging.WARNING) -> None:
         self._cups = cups
         self._loop = None
+        self._experimental = experimental
         logging.getLogger().setLevel(log_level)
+        logging.getLogger("aiopvpc").setLevel(logging.ERROR)
 
         if platform == 'datadis':
             self._conn = DatadisConnector (username, password, log_level=log_level)
         else:
             raise PlatformError (f'platform {platform} not supported, valid options are {PLATFORMS}')
 
+        self._last_update = datetime (1970, 1, 1)
+        if self._experimental:
+            self._pvpc = PVPCData(tariff=TARIFFS[0], local_timezone='Europe/Madrid')
+
         for x in ATTRIBUTES:
-            self.attributes[x] = None
+            if self._experimental or not x.startswith("*"):
+                self.attributes[x] = None
 
     async def async_update (self):
+
+        if self._experimental:
+            if (datetime.now() - self._last_update).days > 1:
+                self._pvpc_raw = await self._pvpc.async_download_prices_for_range(
+                    datetime (datetime.today ().year, datetime.today ().month, 1, 0, 0, 0) - relativedelta(months=1), datetime.today())
+
         if self._loop is None:
             self._loop = asyncio.get_event_loop()
         self._loop.run_in_executor(None, self.update)
+
 
     def update (self):
         date_from = datetime (
@@ -75,7 +95,8 @@ class ReportHelper ():
         date_to = datetime.today()
         if self.update_data (self._cups, date_from, date_to):
             self.data = self._conn.data
-            self.update_attributes ()
+            self.status = self._conn.status
+            self.process_data ()
 
     def update_data (self, cups, date_from=None, date_to=None):
         updated = False
@@ -86,8 +107,8 @@ class ReportHelper ():
             _LOGGER.exception (e)
         return updated
 
-    def update_attributes (self):
-        for f in [self.update_attr_supplies, self.update_attr_contracts, self.update_attr_consumptions, self.update_attr_maximeter]:
+    def process_data (self):
+        for f in [self.process_supplies, self.process_contracts, self.process_consumptions, self.process_maximeter, self.process_pvpc]:
             try:
                 f()
             except Exception as e:
@@ -98,13 +119,13 @@ class ReportHelper ():
             if a in ATTRIBUTES and ATTRIBUTES[a] is not None:
                 self.attributes[a] = round(self.attributes[a], 2) if self.attributes[a] is not None else None
 
-    def update_attr_supplies (self):
+    def process_supplies (self):
         for i in self.data['supplies']:
             if i['cups'] == self._cups:
                 self.attributes['cups'] = self._cups
                 break
 
-    def update_attr_contracts (self):
+    def process_contracts (self):
         most_recent_date = datetime (1970, 1, 1)
         for i in self.data['contracts']:
             if i['date_end'] > most_recent_date:
@@ -113,58 +134,77 @@ class ReportHelper ():
                 self.attributes['contract_p2_kW'] = i.get('power_p2', None)
                 break
 
-    def update_attr_consumptions (self):        
-        processor = ConsumptionProcessor (self.data['consumptions'])
+    def process_consumptions (self):        
+        if len(self.data['consumptions']) > 0:
+            processor = ConsumptionProcessor (self.data['consumptions'])
 
-        today_starts = datetime (
-            datetime.today ().year, 
-            datetime.today ().month, 
-            datetime.today ().day, 0, 0, 0
-        )
-        
-        # update yesterday
-        a = processor.get_stats (today_starts-timedelta(days=1), today_starts)
-        self.attributes["yesterday_kWh"] = a.get('total_kWh', None)
-        self.attributes["yesterday_p1_kWh"] = a.get('p1_kWh', None)
-        self.attributes["yesterday_p2_kWh"] = a.get('p2_kWh', None)
-        self.attributes["yesterday_p3_kWh"] = a.get('p3_kWh', None)
+            today_starts = datetime (
+                datetime.today ().year, 
+                datetime.today ().month, 
+                datetime.today ().day, 0, 0, 0
+            )
 
-        cycle_starts = datetime (
-            datetime.today ().year, 
-            datetime.today ().month, 1, 0, 0, 0
-        )
+            self.data['consumptions_daily_sum'] = processor.group_by (key='D', dt_from=today_starts-timedelta(days=30), action='sum')
+            self.data['consumptions_monthly_sum'] = processor.group_by (key='M', action='sum')
 
-        # update current cycle
-        a = processor.get_stats (cycle_starts, cycle_starts + relativedelta(months=1))
-        self.attributes["month_kWh"] = a.get('total_kWh', None)
-        self.attributes["month_days"] = a.get('days', None)
-        self.attributes["month_daily_kWh"] = a.get('daily_kWh', None)
-        self.attributes["month_p1_kWh"] = a.get('p1_kWh', None)
-        self.attributes["month_p2_kWh"] = a.get('p2_kWh', None)
-        self.attributes["month_p3_kWh"] = a.get('p3_kWh', None)
+            # update yesterday
+            p = processor.process_range (today_starts-timedelta(days=1), today_starts)
+            self.attributes["yesterday_kWh"] = p.get('total_kWh', None)
+            self.attributes["yesterday_p1_kWh"] = p.get('p1_kWh', None)
+            self.attributes["yesterday_p2_kWh"] = p.get('p2_kWh', None)
+            self.attributes["yesterday_p3_kWh"] = p.get('p3_kWh', None)
 
-        # update last cycle
-        a = processor.get_stats (cycle_starts - relativedelta (months=1), cycle_starts)
-        self.attributes["last_month_kWh"] = a.get('total_kWh', None)
-        self.attributes["last_month_days_kWh"] = a.get('days', None)
-        self.attributes["last_month_daily_kWh"] = a.get('daily_kWh', None)
-        self.attributes["last_month_p1_kWh"] = a.get('p1_kWh', None)
-        self.attributes["last_month_p2_kWh"] = a.get('p2_kWh', None)
-        self.attributes["last_month_p3_kWh"] = a.get('p3_kWh', None)
+            # update current month
+            month_starts = datetime (
+                datetime.today ().year, 
+                datetime.today ().month, 1, 0, 0, 0
+            )
 
-    def update_attr_maximeter (self):
+            p = processor.process_range (month_starts, month_starts + relativedelta(months=1))
+            self.attributes["month_kWh"] = p.get('total_kWh', None)
+            self.attributes["month_days"] = p.get('days', None)
+            self.attributes["month_daily_kWh"] = p.get('daily_kWh', None)
+            self.attributes["month_p1_kWh"] = p.get('p1_kWh', None)
+            self.attributes["month_p2_kWh"] = p.get('p2_kWh', None)
+            self.attributes["month_p3_kWh"] = p.get('p3_kWh', None)
 
+            # update last month
+            p = processor.process_range (month_starts - relativedelta (months=1), month_starts)
+            self.attributes["last_month_kWh"] = p.get('total_kWh', None)
+            self.attributes["last_month_days_kWh"] = p.get('days', None)
+            self.attributes["last_month_daily_kWh"] = p.get('daily_kWh', None)
+            self.attributes["last_month_p1_kWh"] = p.get('p1_kWh', None)
+            self.attributes["last_month_p2_kWh"] = p.get('p2_kWh', None)
+            self.attributes["last_month_p3_kWh"] = p.get('p3_kWh', None)
+            if self._experimental:
+                self.attributes["*last_month_idle_W"] = p.get('idle_avg_W', None)
+
+    def process_maximeter (self):
         date_from = datetime (
                 datetime.today ().year, 
                 datetime.today ().month, 
                 1, 0, 0, 0
             ) - relativedelta (months=12)
         processor = MaximeterProcessor (self.data['maximeter'])
-        a = processor.get_stats (date_from, datetime.today())
-        self.attributes['max_power_kW'] = a.get('peak_kW', None)
-        self.attributes['max_power_date'] = a.get('peak_date', None)
-        self.attributes['max_power_mean_kW'] = a.get('peak_mean_kWh', None)
-        self.attributes['max_power_90perc_kW'] = a.get('peak_tile90_kWh', None)
+        p = processor.process_range (date_from, datetime.today())
+        self.attributes['max_power_kW'] = p.get('peak_kW', None)
+        self.attributes['max_power_date'] = p.get('peak_date', None)
+        self.attributes['max_power_mean_kW'] = p.get('peak_mean_kWh', None)
+        self.attributes['max_power_90perc_kW'] = p.get('peak_tile90_kWh', None)
+
+    def process_pvpc (self):
+        if self._experimental:
+            month_starts = datetime (
+                    datetime.today ().year, 
+                    datetime.today ().month, 1, 0, 0, 0
+                )
+            timezone = str(tzlocal.get_localzone())
+            self.cured_pvpc = [{'datetime': datetime.strptime(x.astimezone(tz.timezone(timezone)).strftime('%Y-%m-%d %H:%M'), '%Y-%m-%d %H:%M'), 'price': self._pvpc_raw[x]} for x in self._pvpc_raw]
+            processor = BillingProcessor (self.data['consumptions'], self.data['contracts'], self.cured_pvpc)
+            p = processor.process_range (month_starts, datetime.today())
+            self.attributes['*month_pvpc_€'] = p.get('total', None)
+            p = processor.process_range (month_starts - relativedelta (months=1), month_starts)
+            self.attributes['*last_month_pvpc_€'] = p.get('total', None)
 
     def __str__(self) -> str:
         return '\n'.join([f'{i}: {self.attributes[i]}' for i in self.attributes])
