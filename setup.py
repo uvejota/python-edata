f3ff2bf6898f8972b033237e42ca8fc5991d0f82
VMG <vmayor93@gmail.com>
2021-08-31 12:26:12 +0200

    bump version to 0.2.14
    * fix bug that created duplicated data

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/uvejota/python-edata'
 EMAIL = 'vmayorg@outlook.es'
 AUTHOR = 'VMG'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2.13'
+VERSION = '0.2.14'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
